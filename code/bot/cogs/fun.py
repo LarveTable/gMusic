fbb6bd455ee0bfdc8ac0d83bede94d6a40fcd3ae
LarveTable <cyorick@gmail.com>
2025-05-30 22:11:51 +0200

    added a fun command

--- code/bot/cogs/fun.py
+++ code/bot/cogs/fun.py
@@ -1,6 +1,8 @@
 import discord
 from discord.ext import commands
 from discord import app_commands
+from cogs.secret_jokes import jokes
+import random
 
 class FunCog(commands.Cog):
     def __init__(self, bot):
@@ -15,6 +17,32 @@ class FunCog(commands.Cog):
             color=discord.Color.red()
         )
         await interaction.response.send_message(embed=embed)
+    
+    # The command issuer gets a random 'Ta mère' joke with his name in it
+    @app_commands.command(name='tamere', description='A random \'Ta mère\' joke.')
+    async def tamere(self, interaction: discord.Interaction, user: discord.User = None):
+
+        """
+        Responds with a random 'Ta mère' joke, optionally directed at a specified user.
+        Parameters:
+        - interaction: The interaction object from Discord.
+        - user: The user to direct the joke at. If not specified, the joke is directed at the command issuer.
+        Returns:
+        - None
+        """
+
+        # If no user is specified, use the command issuer
+        if user is None:
+            user = interaction.user
+        
+        # Get a random joke from the tamere_jokes list
+        joke = random.choice(jokes)
+        
+        # Format the joke with the user's name
+        formatted_joke = user.name + ', ' + joke.format(user=user.name)
+        
+        # Send the embed as a response to the interaction
+        await interaction.response.send_message(formatted_joke, tts=True)
 
 async def setup(bot):
     await bot.add_cog(FunCog(bot))
